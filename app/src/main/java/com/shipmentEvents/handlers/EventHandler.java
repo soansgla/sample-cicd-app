5fb139040528c7c6fcf7a77daa5ca2bb27741892
Gladson Soans <soans.gladson@gmail.com>
2022-06-07 21:11:29 +1000

    remove insecured key

--- app/src/main/java/com/shipmentEvents/handlers/EventHandler.java
+++ app/src/main/java/com/shipmentEvents/handlers/EventHandler.java
@@ -64,10 +64,11 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
     }
 
     public String weakMessageEncryption(String message, String key) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        return new String(cipher.doFinal(message.getBytes()), StandardCharsets.UTF_8);
+        // Cipher cipher = Cipher.getInstance("RSA");
+        // SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        // cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        // return new String(cipher.doFinal(message.getBytes()), StandardCharsets.UTF_8);
+        return ""
     }
 
     public ShopifyShop connectToShopify(String subdomain) {
